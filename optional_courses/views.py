e18a3d81e6b826029e47da264b431093e699e5b7
Katya Vasylieva <katevvasylyeva@gmail.com>
2023-01-15 18:16:03 +0200

    Create view named index for the home page

--- optional_courses/views.py
+++ optional_courses/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
 
-# Create your views here.
+from optional_courses.models import Field, Course, Specialization, UniversityGroup, Student
+
+
+def index(request):
+    """View function for the home page of the site."""
+
+    num_fields = Field.objects.count()
+    num_courses = Course.objects.count()
+    num_specializations = Specialization.objects.count()
+    num_university_groups = UniversityGroup.objects.count()
+    num_students = Student.objects.count()
+
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+
+    context = {
+        "num_fields": num_fields,
+        "num_courses": num_courses,
+        "num_specializations": num_specializations,
+        "num_university_groups": num_university_groups,
+        "num_students": num_students,
+        "num_visits": num_visits + 1,
+    }
+
+    return render(request, "optional_courses/index.html", context=context)
