ee573430b564198b4531d42298f84eaf2c53799f
Katya Vasylieva <katevvasylyeva@gmail.com>
2023-01-15 12:50:09 +0200

    Edit files according to flake8

--- optional_courses/apps.py
+++ optional_courses/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class OptionalCoursesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'optional_courses'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "optional_courses"
