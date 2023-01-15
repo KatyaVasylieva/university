fbbe26447dcd22d46f0d6579c1dac9683de26eff
Katya Vasylieva <katevvasylyeva@gmail.com>
2023-01-15 11:27:10 +0200

    Create models specialization, field, universitygroup, course and student

--- optional_courses/models.py
+++ optional_courses/models.py
@@ -1,3 +1,54 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class Specialization(models.Model):
+    name = models.CharField(max_length=63, unique=True)
+    description = models.TextField()
+
+    class Meta:
+        ordering = ["name"]
+
+
+class Field(models.Model):
+    name = models.CharField(max_length=63, unique=True)
+
+    class Meta:
+        ordering = ["name"]
+
+
+class UniversityGroup(models.Model):
+    short_name = models.CharField(max_length=7, unique=True)
+    specialization = models.ForeignKey(
+        Specialization,
+        on_delete=models.CASCADE,
+        related_name="groups"
+    )
+
+    class Meta:
+        ordering = ["short_name"]
+
+
+class Course(models.Model):
+    title = models.CharField(max_length=255, unique=True)
+    field = models.ManyToManyField(Field, related_name="courses")
+
+    class Meta:
+        ordering = ["title"]
+
+
+class Student(AbstractUser):
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    group = models.ForeignKey(
+        UniversityGroup,
+        on_delete=models.CASCADE,
+        related_name="students"
+    )
+    courses = models.ManyToManyField(
+        Course,
+        related_name="students"
+    )
+
+    class Meta:
+        ordering = ["last_name"]
