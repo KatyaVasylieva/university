d9d19b0661897bf33d3289aa6aee210d31629052
Katya Vasylieva <katevvasylyeva@gmail.com>
2023-01-15 16:19:32 +0200

    Override str method in all the models

--- optional_courses/models.py
+++ optional_courses/models.py
@@ -9,12 +9,18 @@ class Specialization(models.Model):
     class Meta:
         ordering = ["name"]
 
+    def __str__(self):
+        return self.name
+
 
 class Field(models.Model):
     name = models.CharField(max_length=63, unique=True)
 
     class Meta:
         ordering = ["name"]
+
+    def __str__(self):
+        return self.name
 
 
 class UniversityGroup(models.Model):
@@ -28,6 +34,9 @@ class UniversityGroup(models.Model):
     class Meta:
         ordering = ["short_name"]
 
+    def __str__(self):
+        return self.short_name
+
 
 class Course(models.Model):
     title = models.CharField(max_length=255, unique=True)
@@ -35,6 +44,9 @@ class Course(models.Model):
 
     class Meta:
         ordering = ["title"]
+
+    def __str__(self):
+        return self.title
 
 
 class Student(AbstractUser):
@@ -52,3 +64,6 @@ class Student(AbstractUser):
 
     class Meta:
         ordering = ["last_name"]
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} ({self.username})"
