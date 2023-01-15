8c7585845c58e722eb0a0d61250b02d1ca3863bb
Katya Vasylieva <katevvasylyeva@gmail.com>
2023-01-15 16:27:13 +0200

    Register student model to django admin

--- optional_courses/admin.py
+++ optional_courses/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from optional_courses.models import Student
+
+
+@admin.register(Student)
+class StudentAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("group",)
+    fieldsets = UserAdmin.fieldsets + (
+        (("Additional info", {"fields": ("group",)}),)
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            (
+                "Additional info",
+                {
+                    "fields": (
+                        "group",
+                    )
+                },
+            ),
+        )
+    )
