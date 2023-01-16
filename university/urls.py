f119a6c1ad739879c280adf8ce7be686351d04d9
Katya Vasylieva <katevvasylyeva@gmail.com>
2023-01-16 14:17:33 +0200

    Create path named accounts to django.contrib.auth.urls inside projects urls

--- university/urls.py
+++ university/urls.py
@@ -22,4 +22,5 @@ from university import settings
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("optional_courses.urls", namespace="optional-courses")),
+    path("accounts/", include("django.contrib.auth.urls")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
