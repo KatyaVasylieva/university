79c75d871f347868fb0c816eab7406b952ee9b8b
Katya Vasylieva <katevvasylyeva@gmail.com>
2023-01-16 19:55:16 +0200

    Create path for field list page into app urls

--- optional_courses/urls.py
+++ optional_courses/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from optional_courses.views import index
+from optional_courses.views import index, FieldListView
 
 urlpatterns = [
-    path("", index, name="index")
+    path("", index, name="index"),
+    path("fields/", FieldListView.as_view(), name="field-list")
 ]
 
 app_name = "optional_courses"
