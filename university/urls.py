ea80ecb8a631819484c61371b49aedb08ea0d081
Katya Vasylieva <katevvasylyeva@gmail.com>
2023-01-17 11:50:52 +0200

    Add path __debug__ to project urls

--- university/urls.py
+++ university/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("optional_courses.urls", namespace="optional-courses")),
     path("accounts/", include("django.contrib.auth.urls")),
+    path("__debud__/", include("debug_toolbar.urls")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
