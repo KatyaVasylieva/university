30fcfd7bb6591c641faf9d13a1a186a3866a2591
Katya Vasylieva <katevvasylyeva@gmail.com>
2023-01-17 10:56:33 +0200

    Create url for field detail page in app urls

--- optional_courses/urls.py
+++ optional_courses/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from optional_courses.views import index, FieldListView
+from optional_courses.views import index, FieldListView, FieldDetailView
 
 urlpatterns = [
     path("", index, name="index"),
-    path("fields/", FieldListView.as_view(), name="field-list")
+    path("fields/", FieldListView.as_view(), name="field-list"),
+    path("fields/<int:pk>/", FieldDetailView.as_view(), name="field-detail")
 ]
 
 app_name = "optional_courses"
