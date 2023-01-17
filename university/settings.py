e5016c0d2a5847c92e49518e855fc49ed3b25eb2
Katya Vasylieva <katevvasylyeva@gmail.com>
2023-01-17 11:49:54 +0200

    Add debug_toolbar into installed apps, middleware, define internal_ips

--- university/settings.py
+++ university/settings.py
@@ -29,6 +29,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
+
 
 # Application definition
 
@@ -41,10 +45,12 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "optional_courses",
     "crispy_forms",
+    "debug_toolbar",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
