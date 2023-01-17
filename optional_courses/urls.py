fe6b137ad13ec41ecd587d1234bdf83980ddaf45
Katya Vasylieva <katevvasylyeva@gmail.com>
2023-01-17 17:02:32 +0200

    Add path to toggle_course_assignment view inside app urls

--- optional_courses/urls.py
+++ optional_courses/urls.py
@@ -1,14 +1,23 @@
 from django.urls import path
 
-from optional_courses.views import index, FieldListView, FieldDetailView,\
-    CourseListView, CourseDetailView
+from optional_courses.views import index, FieldListView, FieldDetailView, \
+    CourseListView, CourseDetailView, toggle_course_assignment
 
 urlpatterns = [
     path("", index, name="index"),
     path("fields/", FieldListView.as_view(), name="field-list"),
     path("fields/<int:pk>/", FieldDetailView.as_view(), name="field-detail"),
     path("courses/", CourseListView.as_view(), name="course-list"),
-    path("courses/<int:pk>/", CourseDetailView.as_view(), name="course-detail"),
+    path(
+        "courses/<int:pk>/",
+        CourseDetailView.as_view(),
+        name="course-detail"
+    ),
+    path(
+        "courses/<int:pk>/toggle-course-assignment",
+        toggle_course_assignment,
+        name="toggle-course-assignment"
+    )
 ]
 
 app_name = "optional_courses"
