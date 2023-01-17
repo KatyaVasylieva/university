f711283345d7df5d5a5c9520f09909bb30f0f684
Katya Vasylieva <katevvasylyeva@gmail.com>
2023-01-17 12:27:57 +0200

    Add path to courses detail view inside app urls

--- optional_courses/urls.py
+++ optional_courses/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
 
 from optional_courses.views import index, FieldListView, FieldDetailView,\
-    CourseListView
+    CourseListView, CourseDetailView
 
 urlpatterns = [
     path("", index, name="index"),
     path("fields/", FieldListView.as_view(), name="field-list"),
     path("fields/<int:pk>/", FieldDetailView.as_view(), name="field-detail"),
-    path("courses/", CourseListView.as_view(), name="course-list")
+    path("courses/", CourseListView.as_view(), name="course-list"),
+    path("courses/<int:pk>/", CourseDetailView.as_view(), name="course-detail"),
 ]
 
 app_name = "optional_courses"
