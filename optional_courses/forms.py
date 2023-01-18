407fd8e3138a58137608354fa5dd16518e738a92
Katya Vasylieva <katevvasylyeva@gmail.com>
2023-01-18 12:21:04 +0200

    Create form for course fields update

--- optional_courses/forms.py
+++ optional_courses/forms.py
@@ -12,3 +12,14 @@ class CourseCreateForm(forms.ModelForm):
     class Meta:
         model = Course
         fields = "__all__"
+
+
+class CourseUpdateFieldsForm(forms.ModelForm):
+    fields = forms.ModelMultipleChoiceField(
+        queryset=Field.objects.all(),
+        widget=forms.CheckboxSelectMultiple
+    )
+
+    class Meta:
+        model = Course
+        fields = ["fields"]
