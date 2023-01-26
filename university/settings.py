c99cd36def26074895fb9f9055ccf273f42218f6
Katya Vasylieva <katevvasylyeva@gmail.com>
2023-01-26 20:01:56 +0200

    Change DEBUG in settings

--- university/settings.py
+++ university/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
+import os
 from pathlib import Path
 import environ
 env = environ.Env()
@@ -26,7 +27,7 @@ SECRET_KEY = env("SECRET_KEY")
 
 
 # SECURITY WARNING: don"t run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
 ALLOWED_HOSTS = []
 
